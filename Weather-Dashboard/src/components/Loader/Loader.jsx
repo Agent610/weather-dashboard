dab2840817dc6f5a3fdad1c854781804b7ab716b
Agent610 <BhavsarParthD96@Gmail.com>
2026-06-26 19:29:11 -0400

    Change in loader

--- Weather-Dashboard/src/components/Loader/Loader.jsx
+++ Weather-Dashboard/src/components/Loader/Loader.jsx
@@ -1,10 +1,31 @@
 import "./Loader.css";
+import React from "react";
+
+// function Loader() {
+//   return (
+//     <div className="loader-container">
+//       <div className="loader">
+//         <p>Please wait weather data is loading....</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Loader;
 
 function Loader() {
   return (
-    <div className="loader-container">
-      <div className="loader">
-        <p>Please wait weather data is loading....</p>
+    <div
+      style={{
+        position: "fixed",
+        inset: 20,
+        backgroundColor: "#fff",
+        zIndex: 9999,
+      }}
+    >
+      <div className="loader-container">
+        <h1>Please wait weather data is loading...</h1>
+        <span className="circle-preloader"></span>
       </div>
     </div>
   );
